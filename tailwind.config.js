57aab65dad8933b4d78bbb2df164147db22dfa80
Rizkiansyah Alfin <rizkiansyahalfin030603@gmail.com>
2025-12-26 11:39:52 +0700

    Initial commit: Social Portfolio Website with Tailwind CSS and Alpine.js (CDN based)

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'social-green': '#16a34a', // Example: green-600
+      }
+    },
+  },
+  plugins: [],
+}
